cb3ba000239fbd8b9075cde4984ee164130aefd3
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-01-16 16:57:09 -0600

    Add pkg-config support to Findgpiodcxx

--- cmake/Findgpiodcxx.cmake
+++ cmake/Findgpiodcxx.cmake
@@ -1,15 +1,22 @@
-find_path(gpiodcxx_INCLUDE_DIR NAMES gpiod.hpp)
+find_package(PkgConfig)
+pkg_check_modules(PC_gpiodcxx QUIET libgpiod)
 
-message("Include dir: " "${gpiodcxx_INCLUDE_DIR}")
+find_path(gpiodcxx_INCLUDE_DIR
+  NAMES gpiod.hpp
+  PATHS ${PC_gpiodcxx_INCLUDE_DIRS})
 
-find_library(gpiodcxx_LIBRARIES NAMES gpiodcxx)
+find_library(gpiodcxx_LIBRARIES
+  NAMES gpiodcxx
+  PATHS ${PC_gpiodcxx_LIBRARY_DIRS})
 
-#set(gpiodcxx_VERSION ${PC_gpiodcxx_VERSION})
+mark_as_advanced(gpiodcxx_INCLUDE_DIR gpiodcxx_LIBRARIES)
+
+set(gpiodcxx_VERSION ${PC_gpiodcxx_VERSION})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(gpiodcxx
   REQUIRED_VARS gpiodcxx_INCLUDE_DIR
-  VERSION_VAR "1.0")
+  VERSION_VAR gpiodcxx_VERSION)
 
 if(gpiodcxx_FOUND AND NOT gpiodcxx::gpiodcxx)
   add_library(gpiodcxx::gpiodcxx SHARED IMPORTED)
