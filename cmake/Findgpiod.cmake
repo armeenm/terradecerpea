f8d6ea62dd7f567f24ce02de7faea38587734979
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-01-16 17:21:06 -0600

    Slight improvements to Findgpiod

--- cmake/Findgpiod.cmake
+++ cmake/Findgpiod.cmake
@@ -1,5 +1,5 @@
 find_package(PkgConfig)
-pkg_check_modules(PC_gpiod QUIET libgpiod)
+pkg_check_modules(PC_gpiod QUIET gpiod)
 
 # C #
 find_path(gpiod_INCLUDE_DIR
@@ -26,18 +26,21 @@ if(gpiod_FOUND AND NOT gpiod::gpiod)
     INTERFACE_INCLUDE_DIRECTORIES "${gpiod_INCLUDE_DIR}")
 endif()
 
+find_package(PkgConfig)
+pkg_check_modules(PC_gpiodcxx QUIET gpiodcxx)
+
 # CXX #
 find_path(gpiodcxx_INCLUDE_DIR
   NAMES gpiod.hpp
-  PATHS ${PC_gpiod_INCLUDE_DIRS})
+  PATHS ${PC_gpiodcxx_INCLUDE_DIRS})
 
 find_library(gpiodcxx_LIBRARIES
   NAMES gpiodcxx
-  PATHS ${PC_gpiod_LIBRARY_DIRS})
+  PATHS ${PC_gpiodcxx_LIBRARY_DIRS})
 
 mark_as_advanced(gpiodcxx_INCLUDE_DIR gpiodcxx_LIBRARIES)
 
-set(gpiodcxx_VERSION ${PC_gpiod_VERSION})
+set(gpiodcxx_VERSION ${PC_gpiodcxx_VERSION})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(gpiodcxx
