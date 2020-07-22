c4051fc5ea19b2f730c2c7f9c810007caef68962
Armeen Mahdian <amahdia2@illinois.edu>
2020-07-22 13:07:12 -0500

    Attempt to get GPIO working again

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 
 #include <algorithm>
 #include <filesystem>
-#include <gpiod.hpp>
 #include <ilanta/control/pose.hpp>
 #include <ilanta/hal/hw/pca9685.hpp>
+#include <ilanta/hal/hw/gpiod.hpp>
 #include <polhemus.hpp>
 #include <range/v3/algorithm/find.hpp>
 #include <range/v3/view/transform.hpp>
 #include <spdlog/spdlog.h>
 #include <string>
+#include <thread>
+#include <chrono>
 
 #ifdef NDEBUG
 auto constexpr inline is_debug = false;
@@ -34,6 +36,7 @@ auto main(int const argc, char const* const* const argv) -> int {
   auto constexpr i2c_port = 0U;
 
   try {
+    /*
     auto servo = ilanta::PCA9685{ilanta::i2c_path(i2c_port)};
 
     auto err = servo.freq(50);
@@ -43,6 +46,17 @@ auto main(int const argc, char const* const* const argv) -> int {
     err = servo.duty_cycle(0, std::stoi(argv[1]));
     if (err)
       spdlog::error("Failed to set duty_cycle: {}", err.message());
+    */
+
+    for (auto const& chip : gpiod::make_chip_iter())
+      fmt::print("{} [{}], Num. Lines: {}\n", chip.name(), chip.label(), chip.num_lines());
+
+    auto const chip = gpiod::chip{"tegra-gpio"};
+    auto line = chip.get_line(78);
+    line.request({"Terradecerpea", gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+    std::this_thread::sleep_for(1s);
+    line.set_value(1);
+    std::this_thread::sleep_for(1s);
 
   } catch (std::exception const& e) {
     spdlog::error("Exception thrown: {}", e.what());
@@ -52,8 +66,6 @@ auto main(int const argc, char const* const* const argv) -> int {
   auto const model_dir = fmt::format("models/actormodel{}", argv[1]);
 
   // GPIO Test //
-  for (auto const& chip : gpiod::make_chip_iter())
-    fmt::print("{} [{}], Num. Lines: {}\n", chip.name(), chip.label(), chip.num_lines());
 
   // RL Model //
   auto rlmodel = RLModel(model_dir);
